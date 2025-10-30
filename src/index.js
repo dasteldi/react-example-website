49608e06cecfdfc06b56098a903db1c561ffdfbc
noo <ivandev833@gmail.com>
2025-10-30 08:37:21 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App.tsx';
+import Header from './components/Header.tsx'
+import Advantages from "./components/Advantages.tsx"
+import Contact from './components/Contact.tsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div> <Header /> <App /> <Advantages /> <Contact /> </div>);
