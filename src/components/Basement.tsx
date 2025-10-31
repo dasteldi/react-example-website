8cb76cbe3114927baffe0efbe8a39b98f49e6ded
dasteb <ivandev833@gmail.com>
2025-10-31 14:43:35 +0300

    Добавление "подвала" сайта

---
+++ src/components/Basement.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Basement() {
+    return(
+    <footer >
+    <nav>
+        <a href="/">Home</a>
+        <a href="vacancies">Vacansies</a>
+        <a href="privacy">Privacy</a>
+        <a href="https://github.com/dasteldi" target="_blank">GitHub</a>
+    </nav>
+    </footer>
+    );
+}
+
+export default Basement;
